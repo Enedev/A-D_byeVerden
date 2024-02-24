90fc931e7e39a45532dd192a78a7af93316257c5
Neithan <neithanochaco@gmail.com>
2024-02-24 14:52:00 -0500

    first point, without comments

--- Taller 1/Punto1.py
+++ Taller 1/Punto1.py
@@ -9,11 +9,9 @@ columns = 3
 
 solution = [[] for i in range(rows + columns - 1)] # This list is used to store the extracted diagonal elements based on their sum (i + j)
 
-# Especificar la posición de inicio (puedes cambiar estos valores)
-start_row = 2  # Fila de inicio (0-indexed)
-start_column = 2  # Columna de inicio (0-indexed)
+start_row = 2  
+start_column = 2 
 
-# Cambiando el orden de los bucles para iniciar desde la posición especificada
 for i in range(start_row, -1, -1):
     for j in range(i if i == start_row else columns - 1, -1, -1):  # Fixed the starting point for j
         sum = i + j
