dfd03f17d44ba9c818589ccdd2c752ddf885637d
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 21:55:18 -0500

    terminado (neithan sacame del sotano)

--- Taller 1/Punto1.py
+++ Taller 1/Punto1.py
@@ -6,27 +6,26 @@ matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-] # O(1) # O(1)
+] # Complejidad temporal : O(1) # Complejidad espacial : O(1)
 
-rows = 3 # O(1) # O(1)  
-columns = 3 # O(1) # O(1)  
+rows = 3 # Complejidad temporal : O(1) # Complejidad espacial : O(1)  
+columns = 3 # Complejidad temporal : O(1) # Complejidad espacial : O(1)  
 
-solution = [[] for i in range(rows + columns - 1)] # This list is used to store the extracted diagonal elements based on their sum (i + j) # O(n) # O(n) 
+solution = [[] for i in range(rows + columns - 1)] # This list is used to store the extracted diagonal elements based on their sum (i + j) # Complejidad temporal : O(n) # Complejidad espacial : O(n) 
  
-start_row = 2 # O(1) # O(1)  
-start_column = 2 # O(1) # O(1)  
+start_row = 2 # Complejidad temporal : O(1) # Complejidad espacial : O(1)  
+start_column = 2 # Complejidad temporal : O(1) # Complejidad espacial : O(1)  
 #iterates over rows in reverse order
 for i in range(start_row, -1, -1): # O(n)
     # iterates over the columns in reverse order
-    for j in range(i if i == start_row else columns - 1, -1, -1):   # O(n)
+    for j in range(i if i == start_row else columns - 1, -1, -1): # O(n)
         #Calculate the sum of i and j to determine the index in solution
-        sum = i + j # O(n)  
+        sum = i + j # Complejidad temporal : O(n) # Complejidad espacial : O(1)  
         # insert the current element 
-        solution[sum].insert(0, matrix[i][j])  # O(1)  
+        solution[sum].insert(0, matrix[i][j])  # O(1)
 
 #print the zigzag
 for i in solution[::-1]:  # O(n)
     for j in i: # O(n)
-        # Complejidad Temporal: O(1)
         print(j, end=" ") # O(1)
 
