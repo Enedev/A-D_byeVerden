416f5e5cd82d883c3d03beb4b59658fb3b75e80d
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 19:45:57 -0500

    Update Punto1.py

--- Taller 1/Punto1.py
+++ Taller 1/Punto1.py
@@ -7,17 +7,18 @@ matrix = [
 rows = 3
 columns = 3
 
-solution = [[] for i in range(rows + columns - 1)] # This list is used to store the extracted diagonal elements based on their sum (i + j) # O(rows + columns)
+solution = [[] for i in range(rows + columns - 1)] # This list is used to store the extracted diagonal elements based on their sum (i + j) # O(n) # O(n) 
 
-start_row = 2  
+start_row = 2
 start_column = 2 
 
-for i in range(start_row, -1, -1): # O(rows)
-    for j in range(i if i == start_row else columns - 1, -1, -1):  # Fixed the starting point for j # O(columns - i)
+for i in range(start_row, -1, -1): # O(n)
+    for j in range(i if i == start_row else columns - 1, -1, -1):  # Fixed the starting point for j # O(n)
         sum = i + j
         solution[sum].insert(0, matrix[i][j])  # O(1)    
 
-for i in solution[::-1]:  # O(rows + columns)
-    for j in i: # O(min(rows, columns))
+for i in solution[::-1]:  # O(n)
+    for j in i: # O(n)
+        # Complejidad Temporal: O(1)
         print(j, end=" ") # O(1)
 
