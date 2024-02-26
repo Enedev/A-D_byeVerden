2000268d217d618afc645d168c04e119fdcd2c4b
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 20:56:38 -0500

    Update Punto4.py

--- Taller 1/Punto4.py
+++ Taller 1/Punto4.py
@@ -1,16 +1,25 @@
-"""Combinaciones de un Conjunto: Implementa una función recursiva que muestre todas las combinaciones posibles de un conjunto dado. 
-Por ejemplo, dado el conjunto {1,2,3} la función debería retornar todas las subconjuntos posibles."""
+"""
+Combinaciones de un Conjunto: Implementa una función recursiva que muestre todas las combinaciones posibles de un conjunto dado. 
+Por ejemplo, dado el conjunto {1,2,3} la función debería retornar todas las subconjuntos posibles.
+"""
 
 def subconjuntos(inicial, actual=[]):
-  if not inicial:
-      if actual == []:
-        return
-      print(actual)
 
-  else:
-      subconjuntos(inicial[1:], actual + [inicial[0]])
-      subconjuntos(inicial[1:], actual)
+    if not inicial:
+        if actual == []:
+            return
+        print(actual)
 
-#Pruebas
+    else:
+        # Incluye el elemento actual en el subconjunto
+        subconjuntos(inicial[1:], actual + [inicial[0]])
+        
+        # No incluye el elemento actual en el subconjunto
+        subconjuntos(inicial[1:], actual)
+
+# Pruebas
+print("Subconjuntos de [1, 2, 3, 4]:")
 subconjuntos([1, 2, 3, 4])
+
+print("\nSubconjuntos de [1, 2, 3]:")
 subconjuntos([1, 2, 3])
