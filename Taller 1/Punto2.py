09b072af3c4389f78c2c56beabbd52b0f286855f
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 21:58:05 -0500

    terminado :))

--- Taller 1/Punto2.py
+++ Taller 1/Punto2.py
@@ -2,15 +2,15 @@
 #de un número entero
 
 def sum_digits(number):
-    total_sum = 0 # O(1) # O(1)
+    total_sum = 0 # Complejidad temporal : O(1) # Complejidad espacial : O(1)
     
-    # Convert the number to a string 
+    # Convert the number to a string
     for digit in str(number): # O(n)
         # Convert each digit back to an integer and add it to the total sum
-        total_sum += int(digit) # O(1)
+        total_sum += int(digit) # Complejidad temporal : O(1) # Complejidad espacial : O(1)
     
     return total_sum # O(1)
 
-number = 12345 # O(1) # O(1)
+number = 12345 # Complejidad temporal : O(1) # Complejidad espacial : O(1)
 result = sum_digits(number) 
 print(f"La suma de los dígitos de {number} es: {result}") # O(1)
