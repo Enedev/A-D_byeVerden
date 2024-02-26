91660f70dd26683be25af4227951a0eca33ddec2
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 21:24:08 -0500

    terminadas las complejidades :))))

--- Taller 1/Punto3.py
+++ Taller 1/Punto3.py
@@ -4,26 +4,26 @@ def permutaciones(cadena):
     
     if len(cadena) <= 1: # O(1)
         return [cadena] # O(1)
-    resultado = [] # O(1) # Complejidad espacial: O(n)
+    resultado = [] # Complejidad temporal : O(1) # Complejidad espacial: O(n)
 
-    pila = [(cadena, '')] # O(1) # Complejidad espacial: O(n)
+    pila = [(cadena, '')] # Complejidad temporal : O(1) # Complejidad espacial: O(n)
     
     while pila: #O(n)
-        actual, permutacion_parcial = pila.pop() # O(n) # Complejidad espacial:O(n)
+        actual, permutacion_parcial = pila.pop() # Complejidad temporal : O(n) # Complejidad espacial:O(n)
 
-        if len(actual) == 1:#O(n)
+        if len(actual) == 1: # O(n)
             resultado.append(permutacion_parcial + actual) # O(n)
             
         else:#O(n)
             for i in range(len(actual)): # O(n)
-                siguiente_permutacion = actual[:i] + actual[i+1:] # O(n) # Complejidad espacial: O(n)
+                siguiente_permutacion = actual[:i] + actual[i+1:] # Complejidad temporal : O(n) # Complejidad espacial: O(n)
                 pila.append((siguiente_permutacion, permutacion_parcial + actual[i])) #O(n)
 
     return resultado # O(1)
 
 #pruebas
-cadena = "123" # O(1) # O(1)
+cadena = "123" # Complejidad temporal : O(1) # Complejidad temporal : O(1)
 print(permutaciones(cadena))
 
-cadena = "456" # O(1) # O(1)
+cadena = "456" # Complejidad temporal :O(1) # Complejidad temporal : O(1)
 print(permutaciones(cadena))
