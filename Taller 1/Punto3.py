9d8ee3835a5e39337f8ddaea4c2d8d997baf427d
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 21:21:21 -0500

    Update Punto3.py
    terminadas las complejidades :))

--- Taller 1/Punto3.py
+++ Taller 1/Punto3.py
@@ -4,19 +4,19 @@ def permutaciones(cadena):
     
     if len(cadena) <= 1: # O(1)
         return [cadena] # O(1)
-    resultado = [] # O(1) # O(1)
+    resultado = [] # O(1) # Complejidad espacial: O(n)
 
-    pila = [(cadena, '')] # O(1) # O(1)
+    pila = [(cadena, '')] # O(1) # Complejidad espacial: O(n)
     
     while pila: #O(n)
-        actual, permutacion_parcial = pila.pop() # O(n) # O(n)
+        actual, permutacion_parcial = pila.pop() # O(n) # Complejidad espacial:O(n)
 
         if len(actual) == 1:#O(n)
             resultado.append(permutacion_parcial + actual) # O(n)
             
         else:#O(n)
             for i in range(len(actual)): # O(n)
-                siguiente_permutacion = actual[:i] + actual[i+1:] # O(n) # O(n)
+                siguiente_permutacion = actual[:i] + actual[i+1:] # O(n) # Complejidad espacial: O(n)
                 pila.append((siguiente_permutacion, permutacion_parcial + actual[i])) #O(n)
 
     return resultado # O(1)
