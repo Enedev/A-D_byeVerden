98b144d26c00fa0c4bae5b3340525046c02e09e4
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 20:59:28 -0500

    Update Punto3.py

--- Taller 1/Punto3.py
+++ Taller 1/Punto3.py
@@ -1,10 +1,10 @@
 # Punto 3
 # Permutaciones de una Cadena: Escribe una función que genere todas las permutaciones posibles de una cadena de caracteres.
-def permutaciones(cadena): #O()
+def permutaciones(cadena): 
     
-    if len(cadena) <= 1: #O(1)
-        return [cadena] #O(1)
-    resultado = [] #O(1)
+    if len(cadena) <= 1: # O(1)
+        return [cadena] # O(1)
+    resultado = [] # O(1)
 
     pila = [(cadena, '')] #O(1)
     
@@ -19,11 +19,11 @@ def permutaciones(cadena): #O()
                 siguiente_permutacion = actual[:i] + actual[i+1:]#O(n)
                 pila.append((siguiente_permutacion, permutacion_parcial + actual[i]))#O(n)
 
-    return resultado #O(1)
+    return resultado # O(1)
 
 #pruebas
-cadena = "123"
+cadena = "123" # O(1)
 print(permutaciones(cadena))
 
-cadena = "456"
+cadena = "456" # O(1)
 print(permutaciones(cadena))
