5bb06645f41bec55a2dbf10d96d006d467c2a2a4
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 21:03:09 -0500

    Update Punto1.py

--- Taller 1/Punto1.py
+++ Taller 1/Punto1.py
@@ -8,19 +8,19 @@ matrix = [
     [7, 8, 9]
 ]
 
-rows = 3
-columns = 3
+rows = 3 # O(1) # O(1)  
+columns = 3 # O(1) # O(1)  
 
 solution = [[] for i in range(rows + columns - 1)] # This list is used to store the extracted diagonal elements based on their sum (i + j) # O(n) # O(n) 
-
-start_row = 2
-start_column = 2 
+ 
+start_row = 2 # O(1) # O(1)  
+start_column = 2 # O(1) # O(1)  
 #iterates over rows in reverse order
 for i in range(start_row, -1, -1): # O(n)
     # iterates over the columns in reverse order
     for j in range(i if i == start_row else columns - 1, -1, -1):   # O(n)
         #Calculate the sum of i and j to determine the index in solution
-        sum = i + j
+        sum = i + j # O(n)  
         # insert the current element 
         solution[sum].insert(0, matrix[i][j])  # O(1)  
 
