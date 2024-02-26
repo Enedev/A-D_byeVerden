0d9923213d17244c0b7303fc16e1ca786bccb643
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 19:52:42 -0500

    Update Punto2.py

--- Taller 1/Punto2.py
+++ Taller 1/Punto2.py
@@ -1,13 +1,13 @@
 def suma_digitos(numero):
-    suma = 0
+    suma = 0 # O(1) # O(1)
     while numero > 0: # O(n)
-        ultimo_digito = numero % 10 # O(1)
+        ultimo_digito = numero % 10 # O(1) # O(1)
         suma += ultimo_digito # O(1)
-        numero //= 10 # O(1)
+        numero //= 10 # O(1) # O(1)
     return suma # O(1)
 try:
-    numero_entero = int(input("Ingresa un número entero: ")) # O(1)
-    resultado = suma_digitos(numero_entero)
+    numero_entero = int(input("Ingresa un número entero: ")) # O(1) # O(1)
+    resultado = suma_digitos(numero_entero) 
     print(f"La suma de los dígitos de {numero_entero} es: {resultado}") # O(1)
 except ValueError:
     print("Error: Debes ingresar un número entero.") # O(1)
