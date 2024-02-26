78be178cb84ce5bbfbac1a5ed51732bee69dd027
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-02-25 20:11:50 -0500

    Update Punto2.py

--- Taller 1/Punto2.py
+++ Taller 1/Punto2.py
@@ -2,15 +2,15 @@
 #de un número entero
 
 def sum_digits(number):
-    total_sum = 0
+    total_sum = 0 # O(1) # O(1)
     
     # Convert the number to a string to iterate over its digits
-    for digit in str(number):
+    for digit in str(number): # O(n)
         # Convert each digit back to an integer and add it to the total sum
-        total_sum += int(digit)
+        total_sum += int(digit) # O(1)
     
-    return total_sum
+    return total_sum # O(1)
 
-number = 12345
-result = sum_digits(number)
-print(f"La suma de los dígitos de {number} es: {result}")
+number = 12345 # O(1) # O(1)
+result = sum_digits(number) 
+print(f"La suma de los dígitos de {number} es: {result}") # O(1)
