678423e0d70b0ea520b93699897cbfa2691c0ac8
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-03-17 21:19:41 -0500

    finally bigO

--- Taller 2/Calculadora/ui.py
+++ Taller 2/Calculadora/ui.py
@@ -9,75 +9,75 @@ from multiplicativePrinciple import principioMultiplicativo
 
 def determinar_tipo_problema(respuestas):
     # Lógica para determinar el tipo de problema basado en las respuestas dadas
-    if respuestas[0] == 's':
-        if respuestas[1] == 'n':
-            if respuestas[2] == 'n':
-                return "Variación ordinaria"
-            elif respuestas[2] == 's':
-                return "Variación con repetición"
-        elif respuestas[1] == 's':
-            if respuestas[2] == 'n':
-                return "Permutación ordinaria"
-            elif respuestas[2] == 's':
-                return "Permutación con repetición"
+    if respuestas[0] == 's': #O(1)
+        if respuestas[1] == 'n': #O(1)
+            if respuestas[2] == 'n': #O(1)
+                return "Variación ordinaria" #O(1)
+            elif respuestas[2] == 's': O(1)
+                return "Variación con repetición" #O(1)
+        elif respuestas[1] == 's': #O(1)
+            if respuestas[2] == 'n': #O(1)
+                return "Permutación ordinaria" #O(1)
+            elif respuestas[2] == 's': #O(1)
+                return "Permutación con repetición" #O(1)
     else:
-        if respuestas[1] == 'n':
-            if respuestas[2] == 'n':
-                return "Combinación ordinaria"
-            elif respuestas[2] == 's':
-                return "Combinación con repetición"
+        if respuestas[1] == 'n': #O(1)
+            if respuestas[2] == 'n': #O(1)
+                return "Combinación ordinaria" #O(1)
+            elif respuestas[2] == 's': #O(1)
+                return "Combinación con repetición" #O(1)
 
 def ui_AM ():
-  numeros = input("Ingrese el numero de posibilidades para cada uno, separados por coma :")
-  pregunta = input("¿Pueden ocurrir al mismo tiempo? s/n").lower()
-  if pregunta == "s":
-    resultado = principioMultiplicativo(numeros)
+  numeros = input("Ingrese el numero de posibilidades para cada uno, separados por coma :") #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+  pregunta = input("¿Pueden ocurrir al mismo tiempo? s/n").lower() #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+  if pregunta == "s": #O(1)
+    resultado = principioMultiplicativo(numeros) #Complejidad espacial : O(1) # Complejidad temporal : O(n)
   else :
-    resultado = principioAditivo(numeros)
+    resultado = principioAditivo(numeros) #Complejidad espacial : O(1) # Complejidad temporal : O(n)
 
-  print("El resultado es:", resultado)
+  print("El resultado es:", resultado) #O(1)
 
 def main():
     #Interfaz de inputs de usuarios 
-    print("Resolución de problemas de conteo")
-    print("Por favor, responda las siguientes preguntas:")
-    respuesta_AM = input("Necesito resolver un problema con principio aditivo o multiplicativo? (s/n):  ").lower()
-    if respuesta_AM == "s":
-      return ui_AM()
-    respuesta_orden = input("1. ¿Importa el orden? (s/n): ").lower()
-    respuesta_intervienen = input("2. ¿Intervienen todos los elementos? (s/n): ").lower()
-    respuesta_repiten = input("3. ¿Se repiten los elementos? (s/n): ").lower()
+    print("Resolución de problemas de conteo") #O(1)
+    print("Por favor, responda las siguientes preguntas:") #O(1)
+    respuesta_AM = input("Necesito resolver un problema con principio aditivo o multiplicativo? (s/n):  ").lower() #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+    if respuesta_AM == "s": #O(1)
+      return ui_AM() #O(n)
+    respuesta_orden = input("1. ¿Importa el orden? (s/n): ").lower() #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+    respuesta_intervienen = input("2. ¿Intervienen todos los elementos? (s/n): ").lower() #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+    respuesta_repiten = input("3. ¿Se repiten los elementos? (s/n): ").lower() #Complejidad espacial : O(1) # Complejidad temporal : O(1)
 
     # Determinar el tipo de problema de conteo según las respuestas del usuario
-    tipo_problema = determinar_tipo_problema((respuesta_orden, respuesta_intervienen, respuesta_repiten))
-    print("Tipo de problema:", tipo_problema)
+    tipo_problema = determinar_tipo_problema((respuesta_orden, respuesta_intervienen, respuesta_repiten)) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+    print("Tipo de problema:", tipo_problema) O(1)
     # Dependiendo del tipo de problema, se ejecuta la solución correspondiente
     # Por cada tipo_problema hay que especificar alguna pregunta y luego el resultado lo da la función correspondiente
-    if tipo_problema == "Variación ordinaria":
+    if tipo_problema == "Variación ordinaria": O(1)
+        m = int(input("Ingrese el número total de elementos (m): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        n = int(input("Ingrese el número de elementos a seleccionar (n): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        resultado = variacion_sin_repeticion(m, n) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
+    elif tipo_problema == "Variación con repetición": O(1)
+        m = int(input("Ingrese el número total de elementos (m): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        n = int(input("Ingrese el número de veces que se pueden repetir los elementos (n): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        resultado = variacion_con_repeticion(m, n) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
+    elif tipo_problema == "Permutación ordinaria":O(1)
         m = int(input("Ingrese el número total de elementos (m): "))
-        n = int(input("Ingrese el número de elementos a seleccionar (n): "))
-        resultado = variacion_sin_repeticion(m, n)
-    elif tipo_problema == "Variación con repetición":
-        m = int(input("Ingrese el número total de elementos (m): "))
-        n = int(input("Ingrese el número de veces que se pueden repetir los elementos (n): "))
-        resultado = variacion_con_repeticion(m, n)
-    elif tipo_problema == "Permutación ordinaria":
-        m = int(input("Ingrese el número total de elementos (m): "))
-        resultado = permutacion_sin_repeticion(m)
-    elif tipo_problema == "Permutación con repetición":
-        m = int(input("Ingrese el número total de elementos (m): "))
-        elementos_repetidos = list(map(int, input("Ingrese la cantidad de repeticiones de cada elemento separadas por espacios: ").split()))
-        resultado = permutacion_con_repeticion(m, elementos_repetidos)
-    elif tipo_problema == "Combinación ordinaria":
-        m = int(input("Ingrese el número total de elementos (m): "))
-        n = int(input("Ingrese el número de elementos a seleccionar (n): "))
-        resultado = combinacion_sin_repeticion(m, n)
-    elif tipo_problema == "Combinación con repetición":
-        m = int(input("Ingrese el número total de elementos (m): "))
-        n = int(input("Ingrese el número de elementos a seleccionar (n): "))
-        resultado = combinacion_con_repeticion(m, n)
+        resultado = permutacion_sin_repeticion(m) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
+    elif tipo_problema == "Permutación con repetición": O(1)
+        m = int(input("Ingrese el número total de elementos (m): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        elementos_repetidos = list(map(int, input("Ingrese la cantidad de repeticiones de cada elemento separadas por espacios: ").split())) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
+        resultado = permutacion_con_repeticion(m, elementos_repetidos) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
+    elif tipo_problema == "Combinación ordinaria": O(1)
+        m = int(input("Ingrese el número total de elementos (m): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        n = int(input("Ingrese el número de elementos a seleccionar (n): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        resultado = combinacion_sin_repeticion(m, n) O(1) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
+    elif tipo_problema == "Combinación con repetición": O(1)
+        m = int(input("Ingrese el número total de elementos (m): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        n = int(input("Ingrese el número de elementos a seleccionar (n): ")) #Complejidad espacial : O(1) # Complejidad temporal : O(1)
+        resultado = combinacion_con_repeticion(m, n) #Complejidad espacial : O(n) # Complejidad temporal : O(n)
 
-    print("El resultado es:", resultado)
+    print("El resultado es:", resultado) O(1)
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": 
+    main() 
