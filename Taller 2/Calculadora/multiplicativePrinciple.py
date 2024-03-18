3e1f102c88ce7e68e456610f72464be580f122b9
LeonardoG <103149934+LeonardoG2005@users.noreply.github.com>
2024-03-17 20:53:45 -0500

    multiplication principio

---
+++ Taller 2/Calculadora/multiplicativePrinciple.py
@@ -0,0 +1,9 @@
+def principioMultiplicativo(numerosMultiplicativo):
+    listaNumeros = list(map(int, numerosMultiplicativo.split(",")))
+
+    if any(not isinstance(numero, int) for numero in listaNumeros):
+        return "Error, ingresaste un valor no numérico"
+      
+    total = math.prod(listaNumeros)
+
+    return total
